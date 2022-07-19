98cf94be986961747caf205c7062087de20e481b
joseantonio002 <joseantunezpul@gmail.com>
2022-07-19 11:34:37 +0100

    starting js

---
+++ 03-carousel/script.js
@@ -0,0 +1,7 @@
+const photo = document.getElementById("photo");
+const name = document.getElementById("name");
+const subcontext = document.getElementById("subcontext");
+const context = document.getElementById("context");
+const right = document.querySelector(".right");
+const left = document.querySelector(".left");
+const surprise = document.getElementById("surprise");
